7354dcf932ee9acf6b396dc397f6b62934cee3f7
Expos07 <soneso677@gmail.com>
2025-01-21 01:31:40 +0900

    Create Coroutines.lua

---
+++ BrawlDev/Coroutines.lua
@@ -0,0 +1,53 @@
+--💎coroutine create
+local task2 = coroutine.create(function()
+	print("coroutine task")
+end)
+
+--💎coroutine resume
+coroutine.resume(task2)
+
+--💎coroutine wrap
+local task2 = coroutine.wrap(function()
+	print("COROUTINE TASK")
+end)
+
+task2()
+
+coroutine.wrap(function()
+	print("coroutine task")
+end)()
+
+local myFunction = function()
+	print("function")
+end
+
+coroutine.wrap(myFunction)()
+
+local function myFunction()
+	print("function")
+end
+
+coroutine.wrap(myFunction)()
+
+--💎coroutine arguments
+local task2 = coroutine.create(function(num1, num2)
+	print(num1 + num2)
+	print("coroutine task")
+end)
+
+coroutine.resume(task2, 5, 5)
+
+--💎multiple tasks
+local loop1 = coroutine.create(function()
+	while true do
+		print("task 1")
+		task.wait(.01)
+	end
+end)
+
+coroutine.resume(loop1)
+
+while true do
+	print("task 2")
+	task.wait(.01)
+end
