14b40e0c4fd7accd80243f048a70444dbdc12c58
Expos07 <soneso677@gmail.com>
2025-08-15 14:22:40 +0900

    Create Built-In Functions.lua

---
+++ DevKing/Built-In Functions.lua
@@ -0,0 +1,5 @@
+local hotdog = game.Workspace:WaitForChild("Part") -- 무한으로 찾음
+print("Yay we have the hotdog")
+
+local MelonFriend = "NotMyHotdog"
+local Melon = game.Workspace:FindFirstChild(MelonFriend) -- 확인하고 없으면 바로 nil
