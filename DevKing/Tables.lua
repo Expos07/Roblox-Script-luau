a48a03a4c3014f4eadef257c0d9f7d454b1455c0
Expos07 <soneso677@gmail.com>
2025-08-17 04:35:40 +0900

    Create Tables.lua

---
+++ DevKing/Tables.lua
@@ -0,0 +1,33 @@
+local omlette = {"Ham", "Egg", "Cheese"}
+local PlayerPhoneNumbers = {4, 6, 3, 6, 8, 1}
+
+table.remove(PlayerPhoneNumbers,2) -- 삭제
+print(PlayerPhoneNumbers[2])
+
+------------------
+
+local omlette = {"Ham", "Egg", "Cheese"}
+local PlayerPhoneNumbers = {4, 6, 3, 6, 8, 1}
+
+table.sort(PlayerPhoneNumbers) -- 정렬
+print(PlayerPhoneNumbers[1])
+
+------------------
+
+local PlayerPhoneNumbers = {4, 6, 3, 6, 8, 1}
+
+table.concat(PlayerPhoneNumbers," ")
+print(PlayerPhoneNumbers)
+
+------------------
+
+local PlayerPhoneNumbers = {4, 6, 3, 6, 8, 1}
+
+print(table.concat(PlayerPhoneNumbers," "))
+
+------------------
+
+local omlette = {"Ham", "Egg", "Cheese"}
+
+table.concat(omlette," ")
+print(omlette)
