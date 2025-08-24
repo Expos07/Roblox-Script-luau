60f0722a952b654a872dd9a97973818a543b567d
Expos07 <soneso677@gmail.com>
2025-08-24 09:02:15 +0900

    Create Point_Script.lua

---
+++ Dev Hub/Point_Script.lua
@@ -0,0 +1,55 @@
+local pointPart = script.Parent
+
+--- Colors
+local Blue = Color3.fromRGB(0, 0, 255)
+local Green = Color3.fromRGB(0, 255, 0)
+local Red = Color3.fromRGB(255, 0, 0)
+
+--- Points value
+local smallPoints = 10
+local largePoints = 50
+local losePoints = 100
+
+local player = game:GetService("Players")
+
+local function givePoints(player)
+	local currentColor = pointPart.Color
+	
+	local playerStats = player:WaitForChild("leaderstats")
+	local Points = playerStats:WaitForChild("Points")
+
+	if currentColor == Blue then
+		Points.Value += smallPoints
+	elseif currentColor == Green then
+		Points.Value += largePoints
+	else
+		Points.Value -= losePoints
+	end
+	
+	pointPart:Destroy()
+	local playerCharacter = player.Character
+	
+	local Particle = Instance.new("ParticleEmitter")
+	Particle.Color = ColorSequence.new(currentColor)
+	Particle.Parent = playerCharacter:WaitForChild("Head")
+	task.wait(1)
+	Particle:Destroy()
+end
+
+local function partTouched(otherPart)
+	local player = player:GetPlayerFromCharacter(otherPart.Parent)
+	if player then
+		givePoints(player)
+	end
+end
+
+pointPart.Touched:Connect(partTouched)
+
+while true do
+	pointPart.Color = Blue
+	task.wait(3)
+	pointPart.Color = Green
+	task.wait(2)
+	pointPart.Color = Red
+	task.wait(1)
+end
